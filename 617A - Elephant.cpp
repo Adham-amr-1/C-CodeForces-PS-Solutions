378c15a37b08f407a481ddb4b54494477c840c82
Adham Amr <104816206+Adham-amr-1@users.noreply.github.com>
2023-03-30 11:23:57 +0200

    Update 617A - Elephant.cpp

--- 617A - Elephant.cpp
+++ 617A - Elephant.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;  // read input
+
+    int steps = 0;
+    while (x > 0) {
+        if (x >= 5) {
+            x -= 5;
+        }
+        else if (x >= 4) {
+            x -= 4;
+        }
+        else if (x >= 3) {
+            x -= 3;
+        }
+        else if (x >= 2) {
+            x -= 2;
+        }
+        else {
+            x -= 1;
+        }
+        steps++;
+    }
+
+    cout << steps << endl;  // output answer
+
+    return 0;
+}
